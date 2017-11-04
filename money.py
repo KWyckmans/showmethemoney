abb24d954b127a0b68c6aa8b534100859bf932b6
Kevin Wyckmans <kevin@pickmeup.be>
2017-11-04 22:55:30 +0100

    group statements per year/month

--- money.py
+++ money.py
@@ -1,50 +1,165 @@
-'''
+"""
 Parses and processes ING statement csv files.
-'''
-import datetime
+"""
+
 import csv
+import datetime
+import glob
 
-STATEMENTS = []
+import re
+import logging
 
-def statement_file(statement_file):
-    yield Statement('you',0,datetime.datetime.now(),'me')
 
 class IngStatementParser(object):
-    '''Parses statement files from the Belgian ING Bank'''
     def __init__(self):
         pass
 
     def parse(self, statement_file):
-        '''Parses a csv file containing statements'''
-        return [Statement('test', 0, datetime.datetime.now(), 'me')]
+        with open(statement_file) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=';')
+            for row in reader:
+                if row['Bedrag'] is not '':
+                    description = ' '.join(row['Omschrijving'].split())
+
+                    description = self.__strip_amount_from_detail(description)
+                    description = self.__strip_date_from_detail(description)
+                    description = self.__strip_hour_from_detail(description)
+                    if self.__is_visa_statement(description):
+                        description = 'VISA Payment'
+                    else:
+                        parts = description.split('-')
+                        description = parts[0]
+                        if len(parts) > 2:
+                            description = description + parts[2]
+                            description_extra = ' '.join(parts[3:])
+                            recipient = self.__extract_pre_country_details(description_extra)
+                            country = self.__extract_country_code(description_extra)
+
+                    description = ' '.join(row['Omschrijving'].split())
+
+                    yield Statement(description, float(row['Bedrag'].replace(',', '.')),
+                                    datetime.datetime.strptime(row['Boekingsdatum'], "%d/%m/%Y"),
+                                    row['Rekening tegenpartij'],
+                                    Account(row['Naam van de rekening'], row['Rekeningnummer']), row['Munteenheid'],
+                                    ' '.join(row['Detail van de omzet'].split()), ' '.join(row['Bericht'].split()))
+
+    @staticmethod
+    def __extract_country_code(detail):
+        match_country = re.search('\s[A-Z][A-Z][A-Z]\s', detail)
+        if match_country:
+            start = match_country.span()[0] + 1
+            end = match_country.span()[1] - 1
+            return detail[start:end]
+        else:
+            return ''
+
+
+    @staticmethod
+    def __extract_pre_country_details(detail):
+        data = re.split('\s[A-Z][A-Z][A-Z]\s', detail)
+        return data[0]
+
+    @staticmethod
+    def __strip_hour_from_detail(detail):
+        date = re.split('[0-9]+.[0-9]+ uur', detail)
+        return ' '.join(date)
+
+    @staticmethod
+    def __strip_date_from_detail(detail):
+        date = re.split('[0-9]+/[0-9]+', detail)
+        return ' '.join(date)
+
+    @staticmethod
+    def __strip_amount_from_detail(detail):
+        amount = re.split('- [0-9]+,[0-9]+', detail)
+        return ' '.join(amount)
+
+    @staticmethod
+    def __is_visa_statement(detail):
+        if detail.startswith('BCC-ING'):
+            return True
+
+        return False
+
+    def __convert_visa_detail(self, detail):
+        pass
+
 
 class StatementParserFactory(object):
     def __init__(self):
         pass
 
-    def create_parser(self, type):
-        return IngStatementParser()
+    def create_parser(self, parser_type):
+        if parser_type == 'ing':
+            return IngStatementParser()
+        else:
+            raise NotImplementedError('No such parser exists')
+
+
+class Account(object):
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+
+    def __str__(self):
+        return f'{self.name} - {self.number}'
 
 
 class Statement(object):
-    '''The Statement class represents a single entry from your bank statements
-
-    A statement contains:
-    - Description: what the transaction was about
-    - Amount: Amount of money received or transferred,
-    - Timestamp: The date of the transaction
-    '''
-    def __init__(self, description, amount, timestamp, recipient):
+    def __init__(self, description, amount, timestamp, recipient, account, currency='Eur', details='', message=''):
         self.description = description
         self.amount = amount
         self.timestamp = timestamp
         self.recipient = recipient
+        self.account = account
+        self.currency = currency
+        self.details = details
+        self.message = message
+
+    def __str__(self):
+        return f'''{abs(self.amount)} ({self.currency}) | {self.recipient} | {self.description} | {self.timestamp} | {self.account} | {self.details} | {self.message}'''
+
+
+def get_years(statements):
+    return {statement.timestamp.year for statement in statements}
+
+
+def get_months(statements):
+    return {statement.timestamp.month for statement in statements}
+
+
+def get_statements_grouped_per_year_and_month(statements, years, months):
+    grouped_statements = {year: {month: [] for month in months} for year in years}
+
+    for year in years:
+        for month in months:
+            logging.debug("Collecting statements for %s - %s", year, month)
+            grouped_statements[year][month] = \
+                [
+                    statement for statement in statements
+                    if statement.timestamp.year == year
+                       and statement.timestamp.month == month
+                ]
+
+    return grouped_statements
+
 
 def main():
-    STATEMENTS.extend(StatementParserFactory().create_parser('ing').parse(''))
+    logging.basicConfig(format='%(asctime)-15s %(funcName)s %(levelname)s %(message)s')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
-    for statement in statement_file(None):
-        print(statement.description)
+    statements = []
+    for file in glob.glob("*.csv"):
+        logger.info("Processing file %s", file)
+        statements.extend(StatementParserFactory().create_parser('ing').parse(file))
+
+    years = get_years(statements)
+    months = get_months(statements)
+    grouped_statements = get_statements_grouped_per_year_and_month(statements, years, months)
+
+    for statement in grouped_statements[2016][1]:
+        logger.debug(statement)
 
 if __name__ == '__main__':
     main()
--- money_test.py
+++ money_test.py
@@ -1,28 +1,28 @@
 import os
 import datetime
 
-import pytest
+import unittest
 import money
 
 
-@pytest.fixture(scope='session')
-def statement_file(tmpdir_factory):
-    temp_file = tmpdir_factory.mkdir("statements").join("statements.csv")
-    temp_file.write("test,20160101,200.12")
-    temp_file.write("test,20160101,-100")
+class MoneyTest(unittest):
+    def statement_file(tmpdir_factory):
+        temp_file = tmpdir_factory.mkdir("statements").join("statements.csv")
+        temp_file.write("test,20160101,200.12")
+        temp_file.write("test,20160101,-100")
 
-    return temp_file
+        return temp_file
 
 
-def test_statement_initializes():
-    statement = money.Statement(description="Carrefour",
-                                amount=20.32, timestamp=datetime.datetime.now(), recipient="Carrefour")
+    def test_statement_initializes():
+        statement = money.Statement(description="Carrefour",
+                                    amount=20.32, timestamp=datetime.datetime.now(), recipient="Carrefour")
 
-    assert statement.description == "Carrefour"
+        assert statement.description == "Carrefour"
 
-def test_load_statements_from_file():
-    parser_factory = money.StatementParserFactory()
-    parser = parser_factory.create_parser('ing')
-    money.STATEMENTS = parser.parse(statement_file)
+    def test_load_statements_from_file():
+        parser_factory = money.StatementParserFactory()
+        parser = parser_factory.create_parser('ing')
+        money.STATEMENTS = parser.parse(statement_file)
 
-    assert len(money.STATEMENTS) > 0
+        assert len(money.STATEMENTS) > 0
